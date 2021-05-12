3e8309dcc615fb474ffebf5814499917ecfc265f
vishnugopy <vishnugoby0010@gmail.com>
2021-05-12 19:13:34 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,23 @@ const isAuth = require("../middlewares/isAuth")
 
 const router = express.Router();
 
+// Display all tweets after being auth
 router.get("/" ,isAuth , tweetcontroler.findAll );
+// To disconnect
 router.get("/logout" , usercontroller.logout );
+// To create an account via post
 router.post("/signup" , usercontroller.newAccount );
+// To ??????
 router.get("/signup" , usercontroller.signup );
+// To connect
 router.get("/login" , usercontroller.login );
+// To verify if the user is connected
 router.post("/login" , usercontroller.authenticate );
+// To get user profile
 router.get("/username/:iduser" , tweetcontroler.getprofile );
-
+// ?????????
 router.get("/profile/:id" , tweetcontroler.profile );
+// To post a tweet
 router.post("/tweet", isAuth , tweetcontroler.addcontent );
 
 
